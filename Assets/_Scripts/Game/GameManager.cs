03f84801f432a97b8a084cdac271cf3f6c823c43
avalon <yuanqi.shan@aalto.fi>
2018-12-01 12:48:00 +0900

    Created gamemanager and player scripts

---
+++ Assets/_Scripts/Game/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Idle,
+    Event,
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public GameState State { get; private set; }
+    private Player player;
+
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        // Load variables
+        player = GetComponentInChildren<Player>();
+    }
+
+    public void SetState(GameState state)
+    {
+        State = state;
+    }
+    
+    private void Update ()
+    {
+        if (State != GameState.Idle)
+            return;
+
+        player.HandleUpdate();
+	}
+
+    private void FixedUpdate()
+    {
+        if (State != GameState.Idle)
+            return;
+
+        player.HandleFixedUpdate();
+    }
+}
---
+++ Assets/_Scripts/Game/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public void HandleUpdate()
+    {
+
+    }
+
+    public void HandleFixedUpdate()
+    {
+
+    }
+}
