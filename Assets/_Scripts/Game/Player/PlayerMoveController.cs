964df4b4003256c9b10177b5e55d6efd8bcb17fb
avalon <yuanqi.shan@aalto.fi>
2018-12-01 19:15:03 +0900

    Idle & run animations

--- Assets/_Scripts/Game/Player/PlayerMoveController.cs
+++ Assets/_Scripts/Game/Player/PlayerMoveController.cs
@@ -10,9 +10,21 @@ public class PlayerMoveController : PlayerComponent
     [SerializeField] private float speed = 4.0f;
     private Rigidbody rBody;
 
+    [Header("Animation")]
+    [SerializeField] private string idleStateName = "Idle";
+    [SerializeField] private string moveStateName = "Run";
+    [SerializeField] private float moveTransitionTime = 0.2f;
+    [SerializeField] private float stopTransitionTime = 0f;
+    private Animator animator;
+    private string moveBoolName = "Moving";
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource footstepSource;
+
     private void Start()
     {
         rBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     public override void HandleUpdate(Player player)
@@ -27,11 +39,14 @@ public class PlayerMoveController : PlayerComponent
     private void HandleMove(Player player)
     {
         if (player.State != SpiritState.Idle)
+        {
+            Stop();
             return;
+        }
 
         var h = Input.GetAxis(Static.horizontalAxis);
         var v = Input.GetAxis(Static.verticalAxis);
-
+        
         if (h != 0 || v != 0)
         {
             var lookDir = GameManager.Instance.gameCamera.LookDirection;
@@ -40,12 +55,33 @@ public class PlayerMoveController : PlayerComponent
             var dir = Quaternion.Euler(0, -angle, 0) * lookDir;
             transform.LookAt(transform.position + dir);
             rBody.velocity = transform.forward * speed;
-            Moving = true;
+
+            if (!Moving)
+            {
+                animator.CrossFade(moveStateName, moveTransitionTime);
+                Moving = true;
+                // animator.SetBool(moveBoolName, true);
+            }
         }
         else
         {
-            rBody.velocity = new Vector3(0, rBody.velocity.y, 0);
-            Moving = false;
+            Stop();
         }
+    }
+
+    private void Stop()
+    {
+        if (Moving)
+        {
+            // animator.SetBool(moveBoolName, false);
+            animator.CrossFade(idleStateName, stopTransitionTime);
+            Moving = false;
+            rBody.velocity = new Vector3(0, rBody.velocity.y, 0);
+        }
+    }
+
+    public void PlayFootstep()
+    {
+        footstepSource.Play();
     }
 }
