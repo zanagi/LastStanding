ee463358670283786a31bc4f6df9cddb2ef1fa82
avalon <yuanqi.shan@aalto.fi>
2018-12-02 03:02:52 +0900

    Bgm changes

---
+++ Assets/_Scripts/Audio/BGMActivator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMActivator : MonoBehaviour {
+    
+    [SerializeField]
+    private bool instant = false;
+    
+    // Use this for initialization
+    void Start()
+    {
+        var audio = GetComponent<AudioSource>();
+        if (audio)
+            BGMManager.Instance.SetBGM(audio, instant);
+    }
+}
---
+++ Assets/_Scripts/Audio/BGMManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BGMManager : Singleton<BGMManager> {
+    
+    [SerializeField]
+    private float bgmChangeTime = 3.0f;
+
+    [SerializeField]
+    private AudioSource[] bgmPrefabs;
+    private AudioSource currentSource;
+    private bool settingBGM;
+    
+    public void SetBGM(AudioSource bgmPrefab, bool instant = false)
+    {
+        StartCoroutine(SetBGMEnum(bgmPrefab, instant));
+    }
+
+    public void SetBGM(string name, bool instant = false)
+    {
+        for (int i = 0; i < bgmPrefabs.Length; i++)
+        {
+            if (bgmPrefabs[i].name == name)
+            {
+                SetBGM(bgmPrefabs[i], instant);
+                break;
+            }
+        }
+    }
+
+    public void StopBGM()
+    {
+        StartCoroutine(StopCurrentBGM());
+    }
+
+    private IEnumerator SetBGMEnum(AudioSource bgmObject, bool instant = false)
+    {
+        while (settingBGM)
+            yield return null;
+
+        // Check same bgm
+        if (currentSource == bgmObject)
+            yield break;
+
+        settingBGM = true;
+        if (!instant)
+            yield return StopCurrentBGM();
+
+        if (currentSource)
+            Destroy(currentSource);
+
+        currentSource = bgmObject;
+        currentSource.transform.SetParent(transform);
+        currentSource.Play();
+
+        if (!instant)
+            yield return FadeInCurrentBGM();
+        settingBGM = false;
+    }
+
+    private IEnumerator StopCurrentBGM()
+    {
+        if (currentSource)
+        {
+            while (currentSource.volume > 0.0f)
+            {
+                currentSource.volume -= Time.fixedDeltaTime / bgmChangeTime;
+                yield return new WaitForFixedUpdate();
+            }
+            currentSource.volume = 0.0f;
+            Destroy(currentSource);
+        }
+    }
+
+    private IEnumerator FadeInCurrentBGM()
+    {
+        var targetVolume = currentSource.volume;
+        currentSource.volume = 0.0f;
+
+        while (currentSource.volume < targetVolume)
+        {
+            currentSource.volume = Mathf.Min(targetVolume, currentSource.volume + Time.fixedDeltaTime / bgmChangeTime);
+            yield return new WaitForFixedUpdate();
+        }
+        currentSource.volume = targetVolume;
+    }
+}
